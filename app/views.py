5b40825ca7bd6772b0157017dc4a476f622db1a3
mukkanti <mukkantisureshkumar777@gmail.com>
2024-07-09 13:10:52 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -11,7 +11,34 @@ class ProductCrudView(ViewSet):
         jso=Productms(po,many=True)
         return Response(jso.data)
     def retrieve(self,request,pk):
-        po=Product.objects.get(pid=pk)
+        po=Product.objects.get(pk=pk)
         jso=Productms(po)
         return Response(jso.data)
-        
+    def create(self,request):
+        jd=request.data
+        pmso=Productms(data=jd)
+        if pmso.is_valid():
+            pmso.save()
+            return Response({'message':'product is creatd'})
+        return Response({'fail':'product is not created'})
+    def update(self,request,pk):
+        po=Product.objects.get(pk=pk)
+        jso=Productms(po,data=request.data)
+        if jso.is_valid():
+            jso.save()
+            return Response({'update':'data updated'})
+        return Response({'fail':'update is fail'})
+    def partial_update(self,request,pk):
+        po=Product.objects.get(pk=pk)
+        jd=request.data
+        jso=Productms(po,jd,partial=True)
+        if jso.is_valid():
+            jso.save()
+            return Response({'partial':'updated data'})
+        return Response({'fail':'data failed'})
+    def destroy(self,request,pk):
+        #Product.objects.get(pk=pk).delete()
+        po=Product.objects.get(pk=pk)
+        po.delete()
+        return Response({'destroy':'deleted the data'})
+    
